3c2c6e6682cbacf12fa6633cd2fe8cfe44017575
Hoàng <thh.it00@gmail.com>
2020-06-06 20:56:08 +0700

    #ADD something

--- services/db.js
+++ services/db.js
@@ -1,11 +1,11 @@
 const Sequelize = require('sequelize');
 
 //Nhat
-const connectionString = process.env.DATABASE_URL || 'postgres://postgres:1604@localhost:5432/todo07';
+// const connectionString = process.env.DATABASE_URL || 'postgres://postgres:1604@localhost:5432/todo07';
 //Hau
 // const connectionString = process.env.DATABASE_URL || 'postgres://postgres:1604@localhost:5432/todo07';
 //Hoang
-// const connectionString = process.env.DATABASE_URL || 'postgres://postgres:1604@localhost:5432/todo07';
+const connectionString = process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost:5432/transfer';
 const db = new Sequelize(connectionString);
 
 
