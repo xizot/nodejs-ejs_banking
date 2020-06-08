d6f952b23a86a52d46c4cd6800f8b404ea7b55f8
Hoàng <thh.it00@gmail.com>
2020-06-08 22:06:48 +0700

    Fix search customers

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 const bodyParser = require('body-parser');
 const app = express();
 var http = require('http').createServer(app)
