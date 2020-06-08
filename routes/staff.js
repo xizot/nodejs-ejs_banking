18bb4be7048bb78f97fa403fcb6d99b5b0aad6ef
Hoàng <thh.it00@gmail.com>
2020-06-08 21:23:57 +0700

    Add search customer

--- routes/staff.js
+++ routes/staff.js
@@ -2,13 +2,24 @@ const express = require('express');
 const router = express.Router();
 const User = require('../services/user');
 
+var users =null;
 // vi du lay danh sach user
 router.get('/',async (req,res)=>{
-
-    const users = await User.findBySomeThing();
-    return res.render('staff',{users});
+        return  res.render('staff',{users})
+   
 })
 
+router.post('/',async (req, res)=>{
+
+    console.log(req.body.txtCustomer)
+    if(req.body.txtCustomer){
+        users = await User.findBySomeThing( req.body.txtCustomer);
+        // cai nay de tra 1 nguoi. sao xai forEach duoc ?
+        console.log(users);
+        return  res.render('staff',{users})
+    }
+    return res.end('done');
+})
 
         
 module.exports = router;
