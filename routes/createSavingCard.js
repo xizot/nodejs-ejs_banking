571eee7033dfa37b2bdf972e03b92159c7e4eaa1
hau <ptmhau17ck1@gmail.com>
2020-08-18 16:17:13 +0700

    add noti realtime

--- routes/createSavingCard.js
+++ routes/createSavingCard.js
@@ -1,21 +1,22 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const io = require("socket.io-client");
+let socket;
+socket = io("https://dack-17ck1.herokuapp.com");
 
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
+  if (!req.currentUser) return res.redirect("/login");
 
-    if (!req.currentUser) return res.redirect('/login')
+  if (!req.currentUser.email && req.currentUser.permisstion == 0)
+    return res.redirect("/add-mail");
 
-    if (!req.currentUser.email && req.currentUser.permisstion == 0) return res.redirect('/add-mail');
+  if (req.currentUser.token && req.currentUser.permisstion == 0)
+    return res.redirect("/active");
+  if (req.currentUser.isActive == 5) return res.redirect("/alert/blocked");
 
-    if (req.currentUser.token && req.currentUser.permisstion == 0) return res.redirect('/active');
-    if (req.currentUser.isActive == 5) return res.redirect('/alert/blocked')
-
-
-    const id = req.currentUser.id || null;
-    if (!id) return res.end('error');
-
-    return res.render('saving')
-})
-
-
+  const id = req.currentUser.id || null;
+  if (!id) return res.end("error");
+  socket.emit("add-new-noti", "me");
+  return res.render("saving");
+});
 
 module.exports = router;
