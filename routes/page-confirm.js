03b2b773ed342e36fcbe5612273d4a49ed291520
hau <Thh.it00@gmail.com>
2020-08-19 15:27:50 +0700

    fix bug

--- routes/page-confirm.js
+++ routes/page-confirm.js
@@ -1,48 +1,52 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const UserRequest = require('../services/userRequest');
-const User = require('../services/user');
-const Customer = require('../services/customer');
-const { body, validationResult } = require('express-validator');
+const UserRequest = require("../services/userRequest");
+const User = require("../services/user");
+const Customer = require("../services/customer");
+const { body, validationResult } = require("express-validator");
 
-const path = require('path');
-var multer = require('multer')
-var upload = multer({ dest: path.join(__dirname, '..', '/public/img') })
+const path = require("path");
+var multer = require("multer");
+var upload = multer({ dest: path.join(__dirname, "..", "/public/img") });
 
 var errors = [];
 
+router.get("/", async (req, res) => {
+  if (!req.currentUser) return res.redirect("/login");
+  if (req.currentUser && req.currentUser.isActive == 1)
+    return res.redirect("/");
+  const customer = await Customer.getByUserID(req.currentUser.id);
 
+  return res.render("page-confirm", { errors, customer });
+});
 
-router.get('/', async (req, res) => {
-    if (!req.currentUser) return res.redirect('/login')
-    if (req.currentUser && req.currentUser.isActive == 5) return res.redirect('/alert/blocked')
-    if (req.currentUser && req.currentUser.isActive == 1) return res.redirect('/')
-    const customer = await Customer.getByUserID(req.currentUser.id);
+router.post("/", upload.single("avatar"), async (req, res) => {
+  if (!req.currentUser) return res.redirect("/login");
+  // xem về multer package để thêm avatar vào folder ./uploads
+  const { txtName, txtTypeofCard, txtIdcard, txtIssued, avatar } = req.body;
 
-    return res.render('page-confirm', { errors, customer });
-})
-
-router.post('/', upload.single('avatar'), async (req, res) => {
-    if (!req.currentUser) return res.redirect('/login');
-    // xem về multer package để thêm avatar vào folder ./uploads
-    const { txtName, txtTypeofCard, txtIdcard, txtIssued, avatar } = req.body;
-
-    console.log(req.file)
-    const image = req.file.filename;
-    const id = req.currentUser.id;
-    if (txtName) {
-        const found = await User.findByID(id);
-        if (found) {
-            found.displayName = txtName;
-            found.save();
-        }
+  console.log(req.file);
+  const image = req.file.filename;
+  const id = req.currentUser.id;
+  if (txtName) {
+    const found = await User.findByID(id);
+    if (found) {
+      found.displayName = txtName;
+      found.save();
     }
-    const sendRequest = await UserRequest.verifyAccount(id, txtTypeofCard, txtIdcard, txtIssued, image);
+  }
+  const sendRequest = await UserRequest.verifyAccount(
+    id,
+    txtTypeofCard,
+    txtIdcard,
+    txtIssued,
+    image
+  );
 
-    if (sendRequest) {
-        return res.redirect('/transfer');
-    }
-    return res.end("Đã xảy ra lỗi");
-})
+  if (sendRequest) {
+    return res.redirect("/transfer");
+  }
+  return res.end("Đã xảy ra lỗi");
+});
 
 module.exports = router;
